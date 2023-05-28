1c699d1a87ad32251a57cf5859d4bad8432ee0e5
mimrancomsats <66168186+mimrancomsats@users.noreply.github.com>
2023-05-28 23:16:28 +0500

    Update buildtest.py

--- buildtest.py
+++ buildtest.py
@@ -15,9 +15,8 @@ browser = webdriver.Chrome(executable_path=DRIVER_LOCATION, options=chrome_optio
 browser.get("https://ecommerce-playground.lambdatest.io/index.php?route=account/register")
 
 browser.implicitly_wait(0.5)
-time.sleep(5)
+#time.sleep(5)
 first_name = browser.find_element(By.ID, "input-firstname")
-#print(first_name.text)
 first_name.send_keys("FirstName")
 last_name = browser.find_element(By.ID, "input-lastname")
 telephone = browser.find_element(By.ID, "input-telephone")
@@ -25,7 +24,7 @@ email = browser.find_element(By.ID, "input-email")
 
 
 last_name.send_keys("LastName")
-email.send_keys("your-email@example.com")
+email.send_keys("sample_email@data.com")
 telephone.send_keys("+351999888777")
 
 password = browser.find_element(By.ID, "input-password")
@@ -40,5 +39,5 @@ terms.click()
 
 continue_button = browser.find_element(By.XPATH, value="//input[@value='Continue']")
 continue_button.click()
-print(browser.title)
+
 assert browser.title == "Your Account Has Been Created!"
